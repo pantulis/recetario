b6dbee9c9c170db303c576ef92f04c9d2eb52355
Juan Lupion <pantulis@gmail.com>
2013-04-13 23:25:08 +0200

    Fix httb basic authentication in dev mode

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
-  http_basic_authenticate_with :name => ENV['HTTP_AUTH_NAME'], :password => ENV['HTTP_AUTH_PASSWORD']
+  http_basic_authenticate_with :name => ENV['HTTP_AUTH_NAME'], :password => ENV['HTTP_AUTH_PASSWORD'] if Rails.env == 'production'
   
 end
